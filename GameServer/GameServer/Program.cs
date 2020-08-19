4c7d2e01fe61e882f276a05e28b7370887d9ad7e
YiranShu <35574463+YiranShu@users.noreply.github.com>
2020-08-19 15:30:29 +0800

    Add files via upload

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameServer {
+    class Program {
+        static void Main(string[] args) {
+            Console.Title = "Game Server";
+            Server.Start(50, 26950);
+            Console.ReadKey();
+        }
+    }
+}
